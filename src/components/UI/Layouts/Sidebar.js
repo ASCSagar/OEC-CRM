ff88c2011d53f5099fbcc20f6fa981f731ad7aac
ASCSagar <ascsagar08@gmail.com>
2024-06-14 18:54:46 +0530

    Website-Enquriy-Contactus

--- src/components/UI/Layouts/Sidebar.js
+++ src/components/UI/Layouts/Sidebar.js
@@ -2,13 +2,10 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import uiAction from "../../../store/uiStore";
 import CollapsableComponent from "../CollapsableComponent";
-import enqSvg from "../../../assets/img/enquiry.svg";
 import { authAction } from "../../../store/authStore";
 import { deleteFromLocalStorage } from "../../../helpers/helperFunctions";
 function NavBar(props) {
-  const uiData = useSelector((state) => state.uiStore);
   const authData = useSelector((state) => state.authStore);
   const dispatch = useDispatch();
   const locaton = useLocation();
@@ -149,6 +146,67 @@ function NavBar(props) {
                 </NavLink>
               </li>
 
+              <li
+                className={`menu ${
+                  locaton.pathname === "/website-enquiry" ? "active" : ""
+                }`}
+              >
+                <NavLink to="/website-enquiry" className="dropdown-toggle">
+                  <div className="heading">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="feather feather-clipboard"
+                    >
+                      <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
+                      <rect
+                        x="8"
+                        y="2"
+                        width="8"
+                        height="4"
+                        rx="1"
+                        ry="1"
+                      ></rect>
+                    </svg>
+                    <span>Website Enquiry</span>
+                  </div>
+                </NavLink>
+              </li>
+              <li className="menu">
+                <a
+                  href="https://oecindia.com/oeccrm/help/index.html"
+                  className="dropdown-toggle"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div className="">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      stroke-width="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      class="feather feather-help-circle"
+                    >
+                      <circle cx="12" cy="12" r="10"></circle>
+                      <path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path>
+                      <line x1="12" y1="17" x2="12.01" y2="17"></line>
+                    </svg>
+                    <span>Help</span>
+                  </div>
+                </a>
+              </li>
               <li className="menu menu-heading">
                 <div className="heading">
                   <svg
